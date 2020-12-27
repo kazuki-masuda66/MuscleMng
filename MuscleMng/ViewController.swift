51e7123408f390eb67fd7c619fe18af79a12f2e5
kazuki.masuda <m.kazuki0606@gmail.com>
2020-12-27 10:24:57 +0800

    初期コミット

---
+++ MuscleMng/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MuscleMng
+//
+//  Created by Kazuki Masuda on 27/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("Hello world")
+    }
+
+
+}
+
